8205b09b1a338f89ebe6f88436532a430b370763
Chen Kevin <diak_344.chen.kevin@12.c.2023.com>
2024-05-20 17:15:02 +0200

    getAll function works

--- backend/src/routes/item.route.ts
+++ backend/src/routes/item.route.ts
@@ -3,6 +3,13 @@ import { findItemById, getAll } from '../repository/ItemRepository';
 
 const itemRouter = Router();
 
-itemRouter.get('/', getAll);
+itemRouter.get('/', async (req, res) => {
+    try {
+        const items = await getAll();
+        res.status(200).json(items);
+    } catch (error) {
+        res.status(500).json({error: 'Internal Server Error'})
+    }
+});
 
 export default itemRouter;
